fe4aa435b1c04dbea60a7d157cc05a564ed193ed
Edvin Navas <edvin.navas@gmail.com>
2023-07-21 11:07:48 -0600

    UNOCORP-TRANSPORTES.
    Versión 2023-07-21 # 1.

--- client-rest-sms/src/main/java/Control/Ctrl_SMS_OPEN.java
+++ client-rest-sms/src/main/java/Control/Ctrl_SMS_OPEN.java
@@ -10,8 +10,11 @@ import jakarta.xml.soap.SOAPMessage;
 import java.io.ByteArrayInputStream;
 import java.io.Serializable;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Ctrl_SMS_OPEN implements Serializable {
@@ -67,26 +70,26 @@ public class Ctrl_SMS_OPEN implements Serializable {
                 ubicacion.setAddress(lst_Ubicacion[14].replaceAll("Â", ""));
                 lista_ubicaciones.add(ubicacion);
             }
-            
+
             Control_Base_Datos control_base_datos = new Control_Base_Datos();
             conn = control_base_datos.obtener_conexion_mysql();
 
             conn.setAutoCommit(false);
-            
+
             Long ID_SMS_OPEN = control_base_datos.ObtenerLong("SELECT IFNULL(MAX(A.ID_SMS_OPEN),0)+1 MAX_ID FROM SMS_OPEN_ENCABEZADO A", conn);
-            
-            String cadenasql = "INSERT INTO SMS_OPEN_ENCABEZADO (ID_SMS_OPEN, FECHA_ACTUALIZACION, NUMERO_UBICACIONES) VALUES (" 
+
+            String cadenasql = "INSERT INTO SMS_OPEN_ENCABEZADO (ID_SMS_OPEN, FECHA_ACTUALIZACION, NUMERO_UBICACIONES) VALUES ("
                     + ID_SMS_OPEN + ","
                     + "CURRENT_TIMESTAMP" + ","
                     + "0" + ")";
             Statement stmt = conn.createStatement();
             stmt.executeUpdate(cadenasql);
             stmt.close();
-            
+
             Integer CORRELATIVO = 0;
             for (Integer i = 0; i < lista_ubicaciones.size(); i++) {
                 CORRELATIVO++;
-                
+
                 cadenasql = "INSERT INTO SMS_OPEN_DETALLE ("
                         + "ID_SMS_OPEN, "
                         + "CORRELATIVO, "
@@ -128,12 +131,89 @@ public class Ctrl_SMS_OPEN implements Serializable {
                 stmt.executeUpdate(cadenasql);
                 stmt.close();
             }
-            
+
             cadenasql = "UPDATE SMS_OPEN_ENCABEZADO SET NUMERO_UBICACIONES=" + CORRELATIVO + " WHERE ID_SMS_OPEN=" + ID_SMS_OPEN;
             stmt = conn.createStatement();
             stmt.executeUpdate(cadenasql);
             stmt.close();
-            
+
+            SimpleDateFormat dateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
+            cadenasql = "SELECT DISTINCT "
+                    + "V.ID_PAIS, "
+                    + "V.ID_COMPANIA, "
+                    + "V.ID_PLANTA, "
+                    + "V.NUMERO_VIAJE, "
+                    + "V.TIPO_ORDEN_VENTA, "
+                    + "V.NUMERO_ORDEN_VENTA, "
+                    + "STR_TO_DATE(SOD.DATETIME_UBICACION, '%d-%m-%Y %H:%i:%s') FECHA_HORA_UBICACION, "
+                    + "SOD.IMEI, "
+                    + "SOD.LATITUDE LATITUD_UBICACION, "
+                    + "SOD.LONGITUDE LONGITUD_UBICACION, "
+                    + "SOD.LOCATIONDESCRIPTION DESCRIPCION_UBICACION "
+                    + "FROM "
+                    + "VIAJES V "
+                    + "LEFT JOIN TRANSPORTISTA T ON (V.ID_TRANSPORTISTA=T.ID_TRANSPORTISTA) "
+                    + "LEFT JOIN DISPONIBILIDAD D ON (V.ID_TRANSPORTISTA=D.ID_TRANSPORTISTA AND V.ID_VEHICULO=D.ID_VEHICULO AND D.FECHA BETWEEN '" + dateFormat1.format(new Date()) + "' AND '" + dateFormat1.format(new Date()) + "') "
+                    + "LEFT JOIN CABEZAL CD ON (D.ID_CABEZAL=CD.ID_CABEZAL) "
+                    + "LEFT JOIN SMS_OPEN_DETALLE SOD ON (CD.IMEI=SOD.IMEI AND STR_TO_DATE(SOD.DATETIME_UBICACION, '%d-%m-%Y %H:%i:%s') BETWEEN '" + dateFormat1.format(new Date()) + " 00:00:00' AND '" + dateFormat1.format(new Date()) + " 23:59:59') "
+                    + "WHERE "
+                    + "V.FECHA_VIAJE BETWEEN '" + dateFormat1.format(new Date()) + "' AND '" + dateFormat1.format(new Date()) + "' AND "
+                    + "V.ESTADO='ACT' AND "
+                    + "T.RASTREABLE=1 AND "
+                    + "SOD.ID_SMS_OPEN IS NOT NULL";
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(cadenasql);
+            while (rs.next()) {
+                Long ID_PAIS = rs.getLong(1);
+                Long ID_COMPANIA = rs.getLong(2);
+                Long ID_PLANTA = rs.getLong(3);
+                Long NUMERO_VIAJE = rs.getLong(4);
+                String TIPO_ORDEN_VENTA = rs.getString(5);
+                Long NUMERO_ORDEN_VENTA = rs.getLong(6);
+                SimpleDateFormat dateFormat2 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                Date FECHA_HORA = dateFormat2.parse(rs.getString(7));
+                String IMEI = rs.getString(8);
+                String LATITUDE = rs.getString(9);
+                String LONGITUDE = rs.getString(10);
+                String LOCATIONDESCRIPTION = rs.getString(11);
+                Double ETA_HORAS = 0.00;
+                Double EDA_KMS = 0.00;
+
+                SimpleDateFormat dateFormat3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                cadenasql = "INSERT INTO VIAJE_UBICACIONES ("
+                        + "ID_PAIS, "
+                        + "ID_COMPANIA, "
+                        + "ID_PLANTA, "
+                        + "NUMERO_VIAJE, "
+                        + "TIPO_ORDEN_VENTA, "
+                        + "NUMERO_ORDEN_VENTA, "
+                        + "FECHA_HORA, "
+                        + "IMEI, "
+                        + "LATITUDE, "
+                        + "LONGITUDE, "
+                        + "LOCATIONDESCRIPTION, "
+                        + "ETA_HORAS, "
+                        + "EDA_KMS) VALUES ("
+                        + ID_PAIS + ","
+                        + ID_COMPANIA + ","
+                        + ID_PLANTA + ","
+                        + NUMERO_VIAJE + ",'"
+                        + TIPO_ORDEN_VENTA + "',"
+                        + NUMERO_ORDEN_VENTA + ",'"
+                        + dateFormat3.format(FECHA_HORA) + "','"
+                        + IMEI + "','"
+                        + LATITUDE + "','"
+                        + LONGITUDE + "','"
+                        + LOCATIONDESCRIPTION + "',"
+                        + ETA_HORAS + ","
+                        + EDA_KMS + ")";
+                Statement stmt1 = conn.createStatement();
+                stmt1.executeUpdate(cadenasql);
+                stmt1.close();
+            }
+            rs.close();
+            stmt.close();
+
             conn.commit();
             conn.setAutoCommit(true);
 
