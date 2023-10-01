d4ead4d83fac45d4a0d19e3d2e66cb8aedf6f656
Edvin Navas <edvin.navas@gmail.com>
2023-10-01 14:44:19 -0600

    SFC-Transporte.
    - Se agrega funcionalidad para consultar la API de Google y obtener el Tiempo y Distancia estimada de llegada.

--- client-rest-sms/src/main/java/Recurso/MyResource.java
+++ client-rest-sms/src/main/java/Recurso/MyResource.java
@@ -3,6 +3,7 @@ package Recurso;
 import Control.Ctrl_SMS_OPEN;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import java.io.Serializable;
@@ -36,14 +37,18 @@ public class MyResource implements Serializable {
     }
     
     @GET
-    @Path("GoogleDistanceMatrix")
+    @Path("GoogleDistanceMatrix/{departure_time}/{origins}/{destinations}/{key}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String distancematrix() {
+    public String distancematrix(
+            @PathParam("departure_time") String departure_time, 
+            @PathParam("origins") String origins, 
+            @PathParam("destinations") String destinations, 
+            @PathParam("key") String key) {
         String resultado;
 
         try {
             Ctrl_SMS_OPEN ctrl_sms_open = new Ctrl_SMS_OPEN();
-            resultado = ctrl_sms_open.distancematrix("now", "14.601372%2C-90.515170", "13.931052%2C-90.797667", "AIzaSyCKLDaTjpMnm-reYkJpAiW2jrl0L4IzyF0");
+            resultado = ctrl_sms_open.distancematrix(departure_time, origins, destinations, key);
         } catch (Exception ex) {
             resultado = "PROYECTO:client-rest-sms|CLASE:" + this.getClass().getName() + "|METODO:distancematrix()|ERROR:" + ex.toString();
             System.out.println("PROYECTO:client-rest-sms|CLASE:" + this.getClass().getName() + "|METODO:distancematrix()|ERROR:" + ex.toString());
